3284fc92b69097c08d826d5e596d89be93d81af1
DeadMoza <elshamia75@gmail.com>
2023-11-21 12:15:08 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 700,
+      height: 600,
+       webPreferences: {
+        nodeIntegration: true
+       }
+    });
+  
+    win.loadFile('src/index.html');
+    win.webContents.openDevTools();
+  }
+
+  try {
+    require('electron-reloader') (module)
+
+  } catch(_) {}
+
+  app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+      });
+  });
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+  });
